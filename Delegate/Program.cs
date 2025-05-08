a339cfc0d59e300ca0c1d1a3cb50b1de4d051f50
renatoads1 <renato.lacerda>
2025-05-08 15:18:44 -0300

    Adicionar arquivos de projeto.

---
+++ Delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Delegate.services;
+
+delegate void MyDelegate(double x,double y);
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyDelegate myDelegate = CalculationServices.ShowMax;
+        myDelegate += CalculationServices.ShowMin;
+        myDelegate += CalculationServices.ShowSum;
+        myDelegate(10, 20);
+    }
+}
---
+++ Delegate/services/CalculationServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.services
+{
+    public class CalculationServices
+    {
+        public static void ShowMax(double x,double y) { 
+        
+            double max = x > y ? x : y;
+            Console.WriteLine($"Max is {max}");
+        }
+        public static void ShowMin(double x, double y)
+        {
+            double min = x < y ? x : y;
+            Console.WriteLine($"Min is {min}");
+        }
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine($"Sum is {sum}");
+        }
+    }
+}
